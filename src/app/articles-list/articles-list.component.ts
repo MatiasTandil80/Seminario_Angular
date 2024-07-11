e59e8878f6add3e8e6274be2cd4d9af8c660b5c8
matiastandil80 <matiastandil80@gmail.com>
2024-07-11 19:38:19 -0300

    Trabajo Terminado

--- src/app/articles-list/articles-list.component.ts
+++ src/app/articles-list/articles-list.component.ts
@@ -11,73 +11,7 @@ import { ArticleDataService } from '../article-data.service';
 
 export class ArticlesListComponent {
 
- articles: Article[] = []; /*[
-    {
-      name: 'Durazno',
-      type: 'Natural',
-      price: 500,
-      stock: 10,
-      image: 'assets/img/lata-durazno.jpg',
-      clearance: false,
-      quantity: 0,
-    },
-    {
-      name: "Leche Entera",
-      type: "Leche entera pasteurizada de 1 litro",
-      price: 120,
-      stock: 15,
-      image: "assets/img/leche-entera.avif",
-      clearance: true,
-      quantity: 0,
-    },
-    {
-      name: "Pan Integral",
-      type: "Pan de molde integral con semillas, 500g",
-      price: 250,
-      stock: 8,
-      image: "assets/img/pan-integral.jpg",
-      clearance: false,
-      quantity: 0,
-    },
-    {
-      name: "Huevos",
-      type: "Docena de huevos",
-      price: 340,
-      stock: 0,
-      image: "assets/img/huevos.jpg",
-      clearance: false,
-      quantity: 0,
-    },
-    {
-      name: "Pollo Fresco",
-      type: "Pollo entero fresco, 1.5 kg",
-      price: 750,
-      stock: 0,
-      image: "assets/img/pollo-fresco.jpg",
-      clearance: false,
-      quantity: 0,
-    },
-    {
-      name: "Café Molido",
-      type: "Café molido 100% arábica, paquete de 250g",
-      price: 480,
-      stock: 3,
-      image: "assets/img/cafe-molido.jpg",
-      clearance: false,
-      quantity: 0,
-    },
-    {
-      name: "Queso Cheddar",
-      type: "Queso cheddar madurado, 200g",
-      price: 375,
-      stock: 9,
-      image: "assets/img/queso-cheddar.jpg",
-      clearance: false,
-      quantity: 0,
-    }
-  ];*/
-
-
+ articles: Article[] = []; 
 
   constructor(private cart: ArticleCartService,
               private articleDataService: ArticleDataService){
